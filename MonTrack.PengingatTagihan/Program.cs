25fa7242dec01c6d921ec305f8736622212676f4
Raissha Najwa Maharani <raissha.njw@gmail.com>
2026-05-10 01:13:14 +0700

    Implementasi class PengingatTagihan dan Unit_Testing di class Program

--- MonTrack.PengingatTagihan/Program.cs
+++ MonTrack.PengingatTagihan/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using MonTrack_PengingatTagihan;
+
+var watch = Stopwatch.StartNew();
+
+// Simulasi proses 100 tagihan
+for (int i = 0; i < 100; i++)
+{
+    var penagih = new PengingatTagihan("Tagihan ke-" + i, 10000);
+    penagih.Bayar("Air", 12500);
+}
+
+watch.Stop();
+Console.WriteLine($"\n--- Hasil Performa ---");
+Console.WriteLine($"Waktu Eksekusi: {watch.Elapsed.TotalMilliseconds} ms");
