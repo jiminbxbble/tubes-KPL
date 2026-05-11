70ff71a27dd262b4c0e4621d3cde3c9aede6e75e
jiminbxbble <nailaturrahmah2006@gmail.com>
2026-05-11 18:52:05 +0700

    melakukan uji testing

---
+++ PencatatanKeuangan.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using PencatatanKeuangan.Models;
+using PencatatanKeuangan.Repositories;
+using PencatatanKeuangan.Services;
+
+namespace PencatatanKeuangan.Tests
+{
+    public class TransactionTests
+    {
+        [Fact]
+        public void TestInputValid_HarusBerhasil()
+        {
+            // Arrange
+            var repo = new DataRepository<Transaction>();
+            var manager = new TransactionManager(repo);
+
+            // Act
+            manager.RecordTransaction(50000, "Uang Saku", "Dikasih ortu");
+
+            // Assert
+            Assert.Equal(50000, manager.GetCurrentBalance());
+        }
+    }
+}
