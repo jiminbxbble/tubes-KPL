80c54767b1357bb4bf388ba6bfbd62aee22ed210
jiminbxbble <nailaturrahmah2006@gmail.com>
2026-06-22 16:59:50 +0700

    feat: menambahkan antarmuka GUI untuk pencatatan keuangan

---
+++ PencatatanKeuangan/GUI/FormPencatatan.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PencatatanKeuangan.GUI
+{
+    partial class FormPencatatan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNominal = new TextBox();
+            txtDeskripsi = new TextBox();
+            cmbKategori = new ComboBox();
+            btnSimpan = new Button();
+            btnKembali = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(80, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nominal:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(80, 171);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Kategori:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(80, 257);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Deskripsi:";
+            // 
+            // txtNominal
+            // 
+            txtNominal.Location = new Point(88, 109);
+            txtNominal.Name = "txtNominal";
+            txtNominal.Size = new Size(125, 27);
+            txtNominal.TabIndex = 3;
+            // 
+            // txtDeskripsi
+            // 
+            txtDeskripsi.Location = new Point(89, 285);
+            txtDeskripsi.Name = "txtDeskripsi";
+            txtDeskripsi.Size = new Size(125, 27);
+            txtDeskripsi.TabIndex = 4;
+            // 
+            // cmbKategori
+            // 
+            cmbKategori.FormattingEnabled = true;
+            cmbKategori.Location = new Point(88, 196);
+            cmbKategori.Name = "cmbKategori";
+            cmbKategori.Size = new Size(151, 28);
+            cmbKategori.TabIndex = 5;
+            // 
+            // btnSimpan
+            // 
+            btnSimpan.Location = new Point(86, 373);
+            btnSimpan.Name = "btnSimpan";
+            btnSimpan.Size = new Size(94, 29);
+            btnSimpan.TabIndex = 6;
+            btnSimpan.Text = "Simpan";
+            btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // btnKembali
+            // 
+            btnKembali.Location = new Point(289, 373);
+            btnKembali.Name = "btnKembali";
+            btnKembali.Size = new Size(94, 29);
+            btnKembali.TabIndex = 7;
+            btnKembali.Text = "Kembali";
+            btnKembali.UseVisualStyleBackColor = true;
+            // 
+            // FormPencatatan
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnKembali);
+            Controls.Add(btnSimpan);
+            Controls.Add(cmbKategori);
+            Controls.Add(txtDeskripsi);
+            Controls.Add(txtNominal);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormPencatatan";
+            Text = "FormPencatatan";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNominal;
+        private TextBox txtDeskripsi;
+        private ComboBox cmbKategori;
+        private Button btnSimpan;
+        private Button btnKembali;
+    }
+}
---
+++ PencatatanKeuangan/GUI/FormPencatatan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using PencatatanKeuangan.Models;
+using PencatatanKeuangan.Services;
+
+namespace PencatatanKeuangan.GUI
+{
+    public partial class FormPencatatan : Form
+    {
+        private TransactionManager _manager;
+
+        // Constructor untuk menerima manager
+        public FormPencatatan(TransactionManager manager)
+        {
+            InitializeComponent();
+            _manager = manager;
+            SetupKategori();
+        }
+
+        private void SetupKategori()
+        {
+            cmbKategori.Items.Add("Uang Saku");
+            cmbKategori.Items.Add("Gaji");
+            cmbKategori.Items.Add("Makan");
+            cmbKategori.Items.Add("Transport");
+            cmbKategori.SelectedIndex = 0;
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            // SECURE CODING: Validasi Input
+            if (!double.TryParse(txtNominal.Text, out double nominal))
+            {
+                MessageBox.Show("Nominal harus berupa angka!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string kategori = cmbKategori.SelectedItem?.ToString();
+            string deskripsi = txtDeskripsi.Text;
+
+            // EXCEPTION HANDLING: Menangkap error dari DbC
+            try
+            {
+                _manager.RecordTransaction(nominal, kategori, deskripsi);
+                MessageBox.Show("Transaksi berhasil dicatat!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtNominal.Clear();
+                txtDeskripsi.Clear();
+                cmbKategori.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnKembali_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Kembali ke form pemanggil
+        }
+    }
+}
